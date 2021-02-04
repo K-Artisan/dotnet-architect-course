74b8d1a879963cd7261c890590d314f40b32d26d
K-Artisan <836439285@qq.com>
2021-02-04 21:33:49 +0800

    md 版Ocleot 单地址多API

--- dotnet-architect-01/src/012 Ocelot/src/Zhaoxi.AspNetCore31.MicroServiceGateway/Startup.cs
+++ dotnet-architect-01/src/012 Ocelot/src/Zhaoxi.AspNetCore31.MicroServiceGateway/Startup.cs
@@ -29,7 +29,7 @@ namespace Zhaoxi.AspNetCore31.MicroServiceGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot().AddConsul().AddPolly();
+            services.AddOcelot();//.AddConsul().AddPolly();
             //services.AddControllers();
         }
 
