c8318ecbd45d8d24a28807af34a3b56d7f33f857
K-Artisan <836439285@qq.com>
2021-03-27 03:31:39 +0800

    docker: docker发布aspnet core、自定义asp.net core的docker镜像

---
+++ dotnet-architect-01/src/047 docker/Zhaoxi.AspNetCore31.DockerProject/Zhaoxi.AspNetCore31.DockerProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Zhaoxi.AspNetCore31.DockerProject.Models;
+
+namespace Zhaoxi.AspNetCore31.DockerProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _IConfiguration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this._IConfiguration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            this._logger.LogWarning($"This is {this._IConfiguration["CustomMessage"]}");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
